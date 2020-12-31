c83ed16e10107c13b933f69e5206de754e7e4d3e
lnreddy <lnreddy1986@gmail.com>
2020-12-31 19:20:25 +0530

    first commit

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  version = "~> 3.0"
+  region     = "us-east-1"
+  access_key = "AKIAXGRCN53CP3N5U2EQ"
+  secret_key = "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"
+}
---
+++ subnet.tf
@@ -0,0 +1,7 @@
+resource "aws_subnet" "subnet1" {
+  vpc_id = "${aws_vpc.aws.id}"  
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "Privet subnet"
+  }
+}
